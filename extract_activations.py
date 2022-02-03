11af6b0dace7a8e242ca72db96ee4a444a920c4c
Sturla Frydenlund <sturla.frydenlund@gmail.com>
2022-02-03 18:35:23 +0100

    written code to get activations

--- extract_activations.py
+++ extract_activations.py
@@ -1,22 +1,11 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from itertools import product
 import sys
 from PIL import Image
+from os import listdir
 
 import numpy as np
 import torch
-import torchvision
+from torchvision import transforms
 
 from lucent.modelzoo import *
 from lucent.misc.io import show
@@ -27,9 +16,22 @@ import lucent.optvis.transform as transform
 from lucent.misc.channel_reducer import ChannelReducer
 from lucent.misc.io import show
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = inceptionv1(pretrained=True)
-_ = model.to(device).eval()
+
+def transform_image(input_image):
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(
+        0
+    )  # create a mini-batch as expected by the model
+    return input_batch
+
 
 @torch.no_grad()
 def get_layer(model, layer, X):
@@ -39,151 +41,45 @@ def get_layer(model, layer, X):
     return hook.features
 
 
-@objectives.wrap_objective()
-def dot_compare(layer, acts, batch=1):
-    def inner(T):
-        pred = T(layer)[batch]
-        return -(pred * acts).sum(dim=0, keepdims=True).mean()
+def get_all_layers(model, layers, X):
+    hooks = [render.ModuleHook(getattr(model, layer)) for layer in layers]
+    model(X)
+    for hook in hooks:
+        hook.close()
+    return [hook.features for hook in hooks]
 
-    return inner
 
-def render_activation_grid_less_naive(
-    img,
-    model,
-    layer="mixed4d",
-    cell_image_size=60,
-    n_groups=6,
-    n_steps=1024,
-    batch_size=64,
-):
-    # First wee need, to normalize and resize the image
-    img = torch.tensor(np.transpose(img, [2, 0, 1])).to(device)
-    normalize = (
-        transform.preprocess_inceptionv1()
-        if model._get_name() == "InceptionV1"
-        else transform.normalize()
-    )
-    transforms = transform.standard_transforms.copy() + [
-        normalize,
-        torch.nn.Upsample(size=224, mode="bilinear", align_corners=True),
+def get_activations(model, layers, num, path="data/ILSVRC2015/Data/DET/test/"):
+    images = []
+    for file in listdir(path)[:num]:
+        images.append(Image.open(path + file))
+    images = [image for image in images if len(image.getbands()) == 3]
+    images = torch.cat(list(map(transform_image, images)))
+    activations = get_all_layers(model, layers, images)
+    max_activations = [act.max(dim=3)[0].max(dim=2)[0] for act in activations]
+    torch.save(max_activations, "activations/max_activations.pt")
+    mean_activations = [act.mean(dim=[2, 3]) for act in activations]
+    torch.save(mean_activations, "activations/mean_activations.pt")
+    return activations
+
+
+def main():
+    layers = [
+        "inception3a",
+        "inception3b",
+        "inception4a",
+        "inception4b",
+        "inception4c",
+        "inception4d",
+        "inception4e",
+        "inception5a",
+        "inception5b",
     ]
-    transforms_f = transform.compose(transforms)
-    # shape: (1, 3, original height of img, original width of img)
-    img = img.unsqueeze(0)
-    # shape: (1, 3, 224, 224)
-    img = transforms_f(img)
 
-    # Here we compute the activations of the layer `layer` using `img` as input
-    # shape: (layer_channels, layer_height, layer_width), the shape depends on the layer
-    acts = get_layer(model, layer, img)[0]
-    # shape: (layer_height, layer_width, layer_channels)
-    acts = acts.permute(1, 2, 0)
-    # shape: (layer_height*layer_width, layer_channels)
-    acts = acts.view(-1, acts.shape[-1])
-    acts_np = acts.cpu().numpy()
-    nb_cells = acts.shape[0]
+    model = torch.hub.load("pytorch/vision:v0.10.0", "googlenet", pretrained=True)
+    model.eval()
+    get_activations(model, layers, 200)
 
-    # negative matrix factorization `NMF` is used to reduce the number
-    # of channels to n_groups. This will be used as the following.
-    # Each cell image in the grid is decomposed into a sum of
-    # (n_groups+1) images. First, each cell has its own set of parameters
-    #  this is what is called `cells_params` (see below). At the same time, we have
-    # a of group of images of size 'n_groups', which also have their own image parametrized
-    # by `groups_params`. The resulting image for a given cell in the grid
-    # is the sum of its own image (parametrized by `cells_params`)
-    # plus a weighted sum of the images of the group. Each each image from the group
-    # is weighted by `groups[cell_index, group_idx]`. Basically, this is a way of having
-    # the possibility to make cells with similar activations have a similar image, because
-    # cells with similar activations will have a similar weighting for the elements
-    # of the group.
-    if n_groups > 0:
-        reducer = ChannelReducer(n_groups, "NMF")
-        groups = reducer.fit_transform(acts_np)
-        groups /= groups.max(0)
-    else:
-        groups = np.zeros([])
-    # shape: (layer_height*layer_width, n_groups)
-    groups = torch.from_numpy(groups)
 
-    # Parametrization of the images of the groups (we have 'n_groups' groups)
-    groups_params, groups_image_f = param.fft_image(
-        [n_groups, 3, cell_image_size, cell_image_size]
-    )
-    # Parametrization of the images of each cell in the grid (we have 'layer_height*layer_width' cells)
-    cells_params, cells_image_f = param.fft_image(
-        [nb_cells, 3, cell_image_size, cell_image_size]
-    )
-
-    # First, we need to construct the images of the grid
-    # from the parameterizations
-
-    def image_f():
-        groups_images = groups_image_f()
-        cells_images = cells_image_f()
-        X = []
-        for i in range(nb_cells):
-            x = 0.7 * cells_images[i] + 0.5 * sum(
-                groups[i, j] * groups_images[j] for j in range(n_groups)
-            )
-            X.append(x)
-        X = torch.stack(X)
-        return X
-
-    # make sure the images are between 0 and 1
-    image_f = param.to_valid_rgb(image_f, decorrelate=True)
-
-    # After constructing the cells images, we sample randomly a mini-batch of cells
-    # from the grid. This is to prevent memory overflow, especially if the grid
-    # is large.
-    def sample(image_f, batch_size):
-        def f():
-            X = image_f()
-            inds = torch.randint(0, len(X), size=(batch_size,))
-            inputs = X[inds]
-            # HACK to store indices of the mini-batch, because we need them
-            # in objective func. Might be better ways to do that
-            sample.inds = inds
-            return inputs
-
-        return f
-
-    image_f_sampled = sample(image_f, batch_size=batch_size)
-
-    # Now, we define the objective function
-
-    def objective_func(model):
-        # shape: (batch_size, layer_channels, cell_layer_height, cell_layer_width)
-        pred = model(layer)
-        # use the sampled indices from `sample` to get the corresponding targets
-        target = acts[sample.inds].to(pred.device)
-        # shape: (batch_size, layer_channels, 1, 1)
-        target = target.view(target.shape[0], target.shape[1], 1, 1)
-        dot = (pred * target).sum(dim=1).mean()
-        return -dot
-
-    obj = objectives.Objective(objective_func)
-
-    def param_f():
-        # We optimize the parametrizations of both the groups and the cells
-        params = list(groups_params) + list(cells_params)
-        return params, image_f_sampled
-
-    results = render.render_vis(
-        model,
-        obj,
-        param_f,
-        thresholds=(n_steps,),
-        show_image=False,
-        progress=True,
-        fixed_image_size=cell_image_size,
-    )
-    # shape: (layer_height*layer_width, 3, grid_image_size, grid_image_size)
-    imgs = image_f()
-    imgs = imgs.cpu().data
-    imgs = imgs[:, :, 2:-2, 2:-2]
-    # turn imgs into a a grid
-    grid = torchvision.utils.make_grid(imgs, nrow=int(np.sqrt(nb_cells)), padding=0)
-    grid = grid.permute(1, 2, 0)
-    grid = grid.numpy()
-    render.show(grid)
-    return imgs
+if __name__ == "__main__":
+    main()
