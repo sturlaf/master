c220c78174750a2f37cfd4eb0b8da0b50f3fd041
Sturla Frydenlund <sturla.frydenlund@gmail.com>
2022-03-10 00:27:33 +0100

    made script for calculating information per node

---
+++ calc_info_circles.py
@@ -0,0 +1,114 @@
+import numpy as np
+from ripser import ripser
+from scipy.spatial.distance import pdist, squareform
+from gudhi.clustering.tomato import Tomato
+from umap import UMAP
+from fix_umap_bug import fix_umap_bug
+import pandas as pd
+from tqdm import tqdm
+from circular_cords import get_coords
+import os
+
+
+def calc_info_circles(layer):
+    activity = np.load(f"activations/ILSVRC2015/{layer}.npy")
+    num_of_neurons = activity.shape[1]
+    cluster_info = pd.read_pickle(f"data/clusters/{layer}.pkl")
+    coeff = 47
+    circle_params = []
+    info_per_nodes = []
+    pbar = tqdm(total=len(cluster_info))
+    for index, row in cluster_info.iterrows():
+        cluster = activity[row["cluster_members"]]
+        layout = UMAP(
+            n_components=num_of_neurons,
+            verbose=True,
+            n_neighbors=20,
+            min_dist=0.01,
+            metric="cosine",
+        ).fit_transform(cluster)
+        distance = squareform(pdist(layout, "euclidean"))
+        persistence = ripser(
+            X=distance,
+            maxdim=1,
+            coeff=coeff,
+            do_cocycles=True,
+            distance_matrix=True,
+            thresh=np.max(distance[~np.isinf(distance)]),
+        )
+        diagrams, cocycles = persistence["dgms"][1], persistence["cocycles"][1]
+        births1, deaths1 = diagrams[:, 0], diagrams[:, 1]
+        lives1 = deaths1 - births1  # the lifetime for the 1-dim classes
+        iMax = np.argsort(lives1)
+        threshold = births1[iMax[-1]] + (deaths1[iMax[-1]] - births1[iMax[-1]]) * (
+            9 / 10
+        )
+        f, theta_matrix, verts, num_verts = get_coords(
+            cocycle=cocycles[iMax[-1]],
+            threshold=threshold,
+            num_sampled=row["cluster_size"],
+            dists=distance,
+            coeff=coeff,
+            bool_smooth_circle="perea",  # "graph", "old"
+        )
+        circle_params.append(f)
+        information_per_node = information_rate(
+            cluster=cluster, theta=f, neurons=num_of_neurons
+        )
+        info_per_nodes.append(information_per_node)
+        pbar.update(1)
+    pbar.close()
+    cluster_info = cluster_info.assign(circle_param=circle_params)
+    cluster_info = cluster_info.assign(info_per_node=info_per_nodes)
+    return cluster_info
+
+
+def inform_rate(mean_n, data, coun):
+    return np.sum(data * np.log2((data / mean_n) + 0.0000001) * coun)
+
+
+def information_rate(cluster, theta, neurons):
+    circ_rates = np.zeros([neurons, 50])
+    counts, bins = np.histogram(theta, bins=50, density=True)
+    for b in range(len(bins) - 1):
+        for n in range(neurons):
+            rates = []
+            for x in range(len(cluster)):
+                if bins[b] < theta[x] <= bins[b + 1]:
+                    rates.append(cluster[x, n])
+            if rates:
+                circ_rates[n, b] = np.mean(rates)
+            else:
+                circ_rates[n, b] = 0
+
+    mean = np.mean(cluster, axis=0)
+
+    return np.array(
+        [inform_rate(mean[n], circ_rates[n, :], counts) for n in range(neurons)]
+    )
+
+
+def main():
+    fix_umap_bug()
+    layers = [
+        # "inception3a",
+        "inception3b",
+        "inception4a",
+        "inception4b",
+        "inception4c",
+        "inception4d",
+        "inception4e",
+        "inception5a",
+        "inception5b",
+    ]
+    save_location = "data/clusters/perera/"
+    if not os.path.exists(save_location):
+        os.makedirs(save_location)
+    for layer in layers:
+        print(f"{layer = }")
+        df = calc_info_circles(layer=layer)
+        df.to_pickle(f"{save_location}{layer}.pkl")
+
+
+if __name__ == "__main__":
+    main()
